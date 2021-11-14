7d9b9e6a63617333deb847cc3a91a5374345ed11
gawor <michal.j.gawor@gmail.com>
2021-11-14 19:23:04 +0100

    uncomment

--- fpProject/src/Main.hs
+++ fpProject/src/Main.hs
@@ -14,14 +14,14 @@ import Kinematics
 import WorldScreen
 import Graphics.Gloss.Interface.IO.Game
 
--- main :: IO ()
--- main = playIO (InWindow "Counter" (600, 600) (0, 0)) -- Or FullScreen
---               black            -- Background color
---               10               -- Frames per second
---               initialModel     -- Initial state
---               view             -- View function
---               input            -- Event function
---               step             -- Step function
+main :: IO ()
+main = playIO (InWindow "Counter" (600, 600) (0, 0)) -- Or FullScreen
+              black            -- Background color
+              10               -- Frames per second
+              initialModel     -- Initial state
+              view             -- View function
+              input            -- Event function
+              step             -- Step function
 
 
 
